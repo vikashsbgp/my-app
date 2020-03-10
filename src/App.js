1ba45734f90ce0212c03394d9561dd6cae3f5a71
vikash singh <vikashsingh@vikashs-MacBook-Air.local>
2020-03-10 16:30:56 +0530

    added error handler page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Person from './Person/Person';
-import './App.css';
+import classes from './App.css';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
 
@@ -46,47 +47,39 @@ class App extends Component {
 
   render() {
 
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      cursor: 'pointer',
-      border: '1px solid blue',
-      padding: '8px',
-      font: 'inherit'
-    };
-
     let persons = null;
+    let btnClass = '';
 
     if (this.state.showPersons) {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person
+            return <ErrorBoundary key={person.id}><Person
                       click={() => this.deletePersonHandler(index)}
                       name={person.name}
                       age={person.age} 
                       key={person.id} 
-                      changed={(event) => {this.nameChangedHandler(event, person.id)}} />
+                      changed={(event) => {this.nameChangedHandler(event, person.id)}} /></ErrorBoundary>
           })}
         </div>
       );
-      style.backgroundColor = 'red';
+      btnClass = classes.Red;
     }
 
-    let classes = [];
+    const assignedClasses = [];
 
     if (this.state.persons.length <= 2) {
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
 
     return(
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi I'm React App</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
-        <button style={style} onClick={this.togglePersons}>Toggle Persons</button>
+        <p className={assignedClasses.join(' ')}>This is really working!</p>
+        <button className={btnClass} onClick={this.togglePersons}>Toggle Persons</button>
         {persons}
       </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import './Person.css';
+import classes from './Person.css';
 
 const person = (props) => {
-
+    const rndNumber = Math.random();
+    if (rndNumber > 2) {
+      throw new Error("Something went wromg.");
+    }
 return (
-    <div className="Person">
+    <div className={classes.Person}>
         <p onClick={props.click}>I'm a {props.name} and I'm {props.age} years old!!</p>
         <p>{props.children}</p>
         <input onChange={props.changed} value={props.name}></input>
