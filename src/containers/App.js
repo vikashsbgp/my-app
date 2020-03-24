348660d4d102eef7b9a6d374200995302b1eea9e
vikash singh <vikashsingh@vikashs-MacBook-Air.local>
2020-03-24 14:08:46 +0530

    segregate files

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import Person from './Person/Person';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
 import classes from './App.css';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
-
 class App extends Component {
 
   state = {
@@ -48,38 +47,24 @@ class App extends Component {
   render() {
 
     let persons = null;
-    let btnClass = '';
 
     if (this.state.showPersons) {
-      persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return <ErrorBoundary key={person.id}><Person
-                      click={() => this.deletePersonHandler(index)}
-                      name={person.name}
-                      age={person.age} 
-                      key={person.id} 
-                      changed={(event) => {this.nameChangedHandler(event, person.id)}} /></ErrorBoundary>
-          })}
-        </div>
-      );
-      btnClass = classes.Red;
+      persons = 
+          <Persons 
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.nameChangedHandler}
+          />
     }
 
-    const assignedClasses = [];
-
-    if (this.state.persons.length <= 2) {
-      assignedClasses.push(classes.red);
-    }
-    if (this.state.persons.length <= 1) {
-      assignedClasses.push(classes.bold);
-    }
 
     return(
       <div className={classes.App}>
-        <h1>Hi I'm React App</h1>
-        <p className={assignedClasses.join(' ')}>This is really working!</p>
-        <button className={btnClass} onClick={this.togglePersons}>Toggle Persons</button>
+        <Cockpit 
+        persons={this.state.persons}
+        showPersons={this.state.showPersons}
+        toggle={this.togglePersons}
+        />
         {persons}
       </div>
     );
